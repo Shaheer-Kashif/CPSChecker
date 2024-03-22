e7681fe3531dc0c2665a4dcc83f68fbbc9eab698
Shaheer Kashif <153809729+Shaheer-Kashif@users.noreply.github.com>
2024-03-22 17:01:32 +0500

    Initial Code Added

---
+++ cpscheck.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.title("CPS Checker")
+
+main_button = Button(root,text="Click Here!",height=20,width=50,bg="white")
+main_button.grid(row=0,column=0)
+
+
+root.mainloop()
