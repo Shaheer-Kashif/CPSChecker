3934e1306153401eac68fb2db502c00cf2c26009
Shaheer Kashif <153809729+Shaheer-Kashif@users.noreply.github.com>
2024-03-27 17:08:51 +0500

    Improved Formatting

--- cpscheck.py
+++ cpscheck.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 # Defining Main Window
 root = Tk()
 root.title("CPS Checker")
-
+root.config(bg = "#8D98A7")
 # Global Check Variables
 click_first = True
 time_first = True
@@ -61,38 +61,43 @@ def click():
 def settimer(sec):
     global time_range
     time_range = sec
+    main_button.config(text="Click Here to Start!\nTimer: "+str(time_range)+"s")
 
-timer_frame = LabelFrame(root,borderwidth=0)
-timer_frame.grid(row=0,column=0)
+timer_frame = LabelFrame(root,borderwidth=0,bg="#8D98A7")
+timer_frame.grid(row=0,column=0,sticky=E)
 
-timer1 = Label(timer_frame,text = "Timer")
+timer_frame.option_add('*Label*background', "#8D98A7")
+
+timer1 = Label(timer_frame,text = "Timer",font=("helvetica",15,"bold"))
 timer1.grid(row=0,column=0)
 
-timer2 = Label(timer_frame,text = "0")
+timer2 = Label(timer_frame,text = "0",font=("helvetica",15))
 timer2.grid(row=1,column=0)
 
-filler = LabelFrame(root,bg="#3C3C3C",width=4,height=50,borderwidth=0)
+filler = LabelFrame(root,bg="#3C3C3C",width=4,height=70,borderwidth=0)
 filler.grid(row=0,column=1)
 
-cpscalc_frame = LabelFrame(root)
-cpscalc_frame.grid(row=0,column=2)
+cpscalc_frame = LabelFrame(root,borderwidth=0,bg="#8D98A7")
+cpscalc_frame.grid(row=0,column=2,sticky=W+E)
 
-cpscheck = Label(cpscalc_frame,text="Click/s")
+cpscalc_frame.option_add('*Label*background', "#8D98A7")
+
+cpscheck = Label(cpscalc_frame,text="Click/s",font=("helvetica",15,"bold"))
 cpscheck.grid(row=0,column=0)
 
-cpscheck2 = Label(cpscalc_frame,text = "0")
+cpscheck2 = Label(cpscalc_frame,text = "0",font=("helvetica",15))
 cpscheck2.grid(row=1,column=0)
 
-main_button = Button(root,text="Click Here to Start!",height=10,width=40,bg="white",command = click,font=("montserrat",18,"bold"))
+main_button = Button(root,text="Click Here to Start!",height=10,width=40,bg="#2C363F",fg="white",command = click,font=("montserrat",18,"bold"))
 main_button.grid(row=1,column=0,columnspan=3)
 
-five_sec_button = Button(root,text="5 Seconds",fg="white",bg="green",command = lambda : settimer(5))
+five_sec_button = Button(root,text="5 Seconds",fg="black",bg="#2CF6B3",width=17,command = lambda : settimer(5),font=("helvetica",15,"bold"))
 five_sec_button.grid(row=2,column=0)
 
-ten_sec_button = Button(root,text="10 Seconds",fg="white",bg="orange",command = lambda : settimer(10))
+ten_sec_button = Button(root,text="10 Seconds",fg="black",bg="#FFB627",width=18,command = lambda : settimer(10),font=("helvetica",15,"bold"))
 ten_sec_button.grid(row=2,column=1)
 
-thirty_sec_button = Button(root,text="30 Seconds",fg="white",bg="red",command = lambda : settimer(30))
+thirty_sec_button = Button(root,text="30 Seconds",fg="black",bg="#DC493A",width=17,command = lambda : settimer(30),font=("helvetica",15,"bold"))
 thirty_sec_button.grid(row=2,column=2)
 
 root.mainloop()
